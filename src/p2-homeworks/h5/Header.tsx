e13d1f1f193ff34465430ba17d6cf1869669056c
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2022-03-23 15:45:23 +0300

    изменил Route на обновленную версию и прописал className по новым правилам

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -19,9 +19,22 @@ const Header = () => {
       {
         navigation
           ? <div>
-            <NavLink to={PATH.PRE_JUNIOR} activeClassName={s.active} className={s.link}>PreJunior</NavLink>
-            <NavLink to={PATH.JUNIOR} activeClassName={s.active} className={s.link}>Junior</NavLink>
-            <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.active} className={`${s.link} ${s.nav}`}>Junior+</NavLink>
+            <NavLink
+              className={(params) => params.isActive ? s.active : s.link}
+              to={PATH.PRE_JUNIOR}
+            >
+              PreJunior
+            </NavLink>
+            <NavLink
+              to={PATH.JUNIOR}
+              className={(params) => params.isActive ? s.active : s.link}>
+              Junior
+            </NavLink>
+            <NavLink
+              to={PATH.JUNIOR_PLUS}
+              className={(params) => params.isActive ? s.active : s.link}>
+              Junior+
+            </NavLink>
             <button onClick={closeMenu} className={s.hamburger}>&#171;</button>
           </div>
           : <button onClick={openMenu} className={s.hamburger}>&#187;</button>
