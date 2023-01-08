87358060eadc78aff186c97c2363fe5400d57308
StanislaVailevich <stanislavasilevich@yandex.ru>
2023-01-09 00:46:36 +0300

    in two lesson completed 1,2,3 tasks

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -3,9 +3,6 @@ import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
 
 /*
-* 1 - описать типы AffairPriorityType, AffairType
-* 2 - указать нужный тип для defaultAffairs
-* 3 - дописать типы и логику функции filterAffairs и проверить её тестами
 * 4 - выполнить пункт 3 для функции deleteAffair
 * 5 - указать нужный тип в useState с affairs
 * 6 - дописать тип и логику функции deleteAffairCallback
@@ -17,17 +14,17 @@ import s2 from '../../s1-main/App.module.css'
 * */
 
 // types
-export type AffairPriorityType = any // need to fix any
+export type AffairPriorityType = 'low' | 'middle' | 'high';
 export type AffairType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: number
+    name: string
     priority: AffairPriorityType
 }
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
-    {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
+const defaultAffairs: Array<AffairType> = [
+    {_id: 1, name: 'React', priority: 'high'},
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
     {_id: 4, name: 'work', priority: 'high'},
@@ -35,8 +32,16 @@ const defaultAffairs: any = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-
+export const filterAffairs = (affairs: Array<AffairType>, filter: FilterType): Array<AffairType> => { // need to fix any
+    if(filter === 'low') {
+        affairs = affairs.filter(i => i.priority === 'low');
+    }
+    if(filter === 'middle') {
+        affairs = affairs.filter(i => i.priority === 'middle');
+    }
+    if(filter === 'high') {
+        affairs = affairs.filter(i => i.priority === 'high');
+    }
 
     return affairs // need to fix
 }
