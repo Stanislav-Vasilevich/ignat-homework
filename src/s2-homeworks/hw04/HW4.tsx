a3650f0d5bfb0e01a1df18bde736edcc3bf21536
StanislaVailevich <stanislavasilevich@yandex.ru>
2023-01-28 16:02:03 +0300

    сделал половину 4-го задания понедельника

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -3,7 +3,6 @@ import s2 from '../../s1-main/App.module.css'
 import Stand from './Stand'
 
 /*
-* 1 - понять (и простить) SuperInputText
 * 2 - в зависимости от типа и дизэйбла прицепить нужный класс в SuperButton.tsx (строка 21)
 * 3 - дописать onChangeCallback в SuperCheckbox.tsx чтоб оба чекбокса работали на стенде
 * 4 - сделать стили в соответствии с дизайном
@@ -12,10 +11,12 @@ import Stand from './Stand'
 const HW4 = () => {
     return (
         <div id={'hw4'}>
-            <div className={s2.hwTitle}>Homework #4</div>
+					<div className={s2.container}>
+						<div className={s2.hwTitle}>Homework №4</div>
+					</div>
             {/*демонстрация возможностей компонент:*/}
             <div className={s2.hw}>
-                <Stand />
+							<Stand />
             </div>
         </div>
     )
