4ae7b351ea3aa20471e6c09da6af498f4d6dcb7e
StanislaVailevich <stanislavasilevich@yandex.ru>
2023-02-16 10:28:54 +0300

    выполнил первое задание вторника

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -3,36 +3,37 @@ import burgerIcon from './burger.svg'
 import s from './Header.module.css'
 import {useLocation} from 'react-router-dom'
 import {PATH} from '../Pages'
+import s2 from "../../../s1-main/App.module.css";
 
 type PropsType = {
-    handleOpen: () => void
+	handleOpen: () => void
 }
 
 export const Header: FC<PropsType> = ({handleOpen}) => {
-    // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
-    const location = useLocation()
-    const currentPath = location.pathname
+	// hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
+	const location = useLocation()
+	const currentPath = location.pathname
 
-    const pageName =
-        currentPath === PATH.PRE_JUNIOR
-            ? 'Pre-junior'
-            : currentPath === PATH.JUNIOR
-                ? 'Junior'
-                : currentPath === PATH.JUNIOR_PLUS
-                    ? 'Junior Plus'
-                    : 'Error'
-    return (
-        <>
-            <div id={'hw5-header'} className={s.header}>
-                <img
-                    src={burgerIcon}
-                    id={'hw5-burger-menu'}
-                    className={s.burgerMenuIcon}
-                    onClick={handleOpen}
-                    alt={'open menu'}
-                />
-                <h1>{pageName}</h1>
-            </div>
-        </>
-    )
+	const pageName =
+		currentPath === PATH.PRE_JUNIOR
+			? 'Pre-junior'
+			: currentPath === PATH.JUNIOR
+				? 'Junior'
+				: currentPath === PATH.JUNIOR_PLUS
+					? 'Junior Plus'
+					: 'Error'
+	return (
+		<header id={'hw5-header'} className={s.header}>
+			<div className={`${s2.container} ${s.content}`}>
+				<img
+					src={burgerIcon}
+					id={'hw5-burger-menu'}
+					className={s.burgerMenuIcon}
+					onClick={handleOpen}
+					alt={'open menu'}
+				/>
+				<h1 className={s.title}>{pageName}</h1>
+			</div>
+		</header>
+	)
 }
